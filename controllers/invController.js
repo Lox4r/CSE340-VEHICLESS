8252778abb119fb6d93f24cd51db6fe5c14379dd
lox4r <loxhand@gmail.com>
2026-04-16 16:54:56 -0500

    1

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,11 +1,23 @@
-invController.buildByInventoryId = async function (req, res, next) {
-  const inv_id = parseInt(req.params.inv_id)
-  const data = await invModel.getInventoryById(inv_id)
-  const nav = await utilities.getNav()
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities")
 
-  res.render("inventory/detail", {
-    title: data.inv_make + " " + data.inv_model,
-    nav,
-    vehicle: data,
-  })
+const invController = {}
+
+// Build vehicle detail view (FIXED)
+invController.buildByInventoryId = async function (req, res, next) {
+  try {
+    const inv_id = parseInt(req.params.inv_id)
+    const data = await invModel.getInventoryById(inv_id)
+    const nav = await utilities.getNav()
+
+    res.render("inventory/detail", {
+      title: data.inv_make + " " + data.inv_model,
+      nav,
+      vehicle: data,
+    })
+  } catch (error) {
+    next(error)
+  }
 }
+
+module.exports = invController
